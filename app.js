eafe8aa4902c09fa01c082efc946217b2676d768
janz-emmanuel24 <jansabbas1@gmail.com>
2020-07-06 14:51:32 +0300

    Added a comment

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
+//start express up 
 const app = express();
 
 app.set('view engine', 'pug');
